c85c9aed7be062e8f7ce610eb917dfa32b237aa9
rizlajf <rizla.jf@gmail.com>
2018-05-21 15:50:21 +0530

    Update remoteUser.model.ts

--- src/app/core/models/remoteUser.model.ts
+++ src/app/core/models/remoteUser.model.ts
@@ -1,6 +1,64 @@
-export interface RemoteUser {
-  firstName: string;
-  lastName: string;
-  Email: string;
-  Phone : string;
-}
+//declare module namespace {
+
+  export interface Name {
+      title: string;
+      first: string;
+      last: string;
+  }
+
+  export interface Location {
+      street: string;
+      city: string;
+      state: string;
+      postcode: number;
+  }
+
+  export interface Login {
+      username: string;
+      password: string;
+      salt: string;
+      md5: string;
+      sha1: string;
+      sha256: string;
+  }
+
+  export interface Id {
+      name: string;
+      value?: any;
+  }
+
+  export interface Picture {
+      large: string;
+      medium: string;
+      thumbnail: string;
+  }
+
+  export interface Result {
+      gender: string;
+      name: Name;
+      location: Location;
+      email: string;
+      login: Login;
+      dob: string;
+      registered: string;
+      phone: string;
+      cell: string;
+      id: Id;
+      picture: Picture;
+      nat: string;
+  }
+
+  export interface Info {
+      seed: string;
+      results: number;
+      page: number;
+      version: string;
+  }
+
+  export interface RemoteUser {
+      results: Result[];
+      info: Info;
+  }
+
+//}
+
